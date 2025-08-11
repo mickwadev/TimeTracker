283ed561edd6b0c6b7666f9e1c640dd79ce86818
mickwa <mickwadev@gmail.com>
2025-08-11 14:40:54 +0200

    Simple timer

--- PageModels/TimeTrackingPageModel.cs
+++ PageModels/TimeTrackingPageModel.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeTracker.Data;
-
+ 
 namespace TimeTracker.PageModels
 {
     public partial class TimeTrackingPageModel : ObservableObject
@@ -15,19 +15,39 @@ namespace TimeTracker.PageModels
         DatabaseFun db;
         private DateTime _startTime, _endTime;
         private bool learningInProgress = false;
+        IDispatcherTimer timer;
+        int ticks = 0;
 
         public TimeTrackingPageModel(DatabaseFun db) 
         {
+            timer = Application.Current!.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.IsRepeating = true;
+            timer.Tick += (s, e) =>
+            {
+                ticks++;
+                TicksMsg = $"Ticks: {ticks}";
+            };
             this.db = db;
         }
 
         [ObservableProperty]
         private string _today = DateTime.Now.ToString("dddd");
 
+        [ObservableProperty]
+        private string _ticksMsg = $"Ticks: -1";
+
         [RelayCommand]
         private void TimeCountingButtonPressed()
         {
             Trace.WriteLine("Time counting...");
+            StartWorkTimeCounting();
+        }
+
+        private void StartWorkTimeCounting()
+        {
+            if(!timer.IsRunning)
+            timer.Start(); 
         }
     }
 }
