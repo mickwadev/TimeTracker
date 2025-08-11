e7a77879749a07fdb346bcdd6c9a7fd657afed0b
mickwa <mickwadev@gmail.com>
2025-08-11 13:29:29 +0200

    Add db functions to create table

---
+++ Data/DatabaseFun.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Data
+{
+    // to jest dodane w builderze do Sevices
+    public class DatabaseFun
+    {
+        const string CurrentTable = "TimesTable";
+        public async Task DropTableAsync()
+        {
+            Trace.WriteLine($"Using connection string: {DbConsts.connectionString}");
+            await using var connection = new SqliteConnection(DbConsts.connectionString);
+            await connection.OpenAsync();
+            var dropTableCommand = connection.CreateCommand();
+            dropTableCommand.CommandText = @$"
+                DROP TABLE IF EXISTS {CurrentTable};";
+            await dropTableCommand.ExecuteNonQueryAsync();
+        }
+
+        public async Task AddTimesTableAsync()
+        {
+            Trace.WriteLine($"Using connection string: {DbConsts.connectionString}");
+            await using var connection = new SqliteConnection(DbConsts.connectionString);
+            await connection.OpenAsync();
+            var createTableCommand = connection.CreateCommand();
+            createTableCommand.CommandText = @$"
+CREATE TABLE IF NOT EXISTS {CurrentTable} (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Title TEXT NOT NULL,
+    StartTime TEXT NOT NULL,
+    EndTime TEXT NOT NULL
+);";
+            await createTableCommand.ExecuteNonQueryAsync();
+        }
+
+        public async Task CheckIfTableExistsAsync()
+        {
+            await using var connection = new SqliteConnection(DbConsts.connectionString);
+            await connection.OpenAsync();
+            var checkIfTableExistCmd = connection.CreateCommand();
+            checkIfTableExistCmd.CommandText = @$"
+            SELECT name FROM sqlite_master WHERE type='table' AND name='{CurrentTable}';";
+            await using var reader = await checkIfTableExistCmd.ExecuteReaderAsync();
+            bool tableExists = await reader.ReadAsync();
+            Trace.WriteLine($"Table {CurrentTable} exists: {tableExists}");
+        }
+    }
+}
---
+++ Data/DbConsts.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeTracker.Data
+{
+    internal class DbConsts
+    {
+        public const string dbName = "MyFunDb.db3";
+        public static string dbPath => Path.Combine(FileSystem.AppDataDirectory, dbName);
+        // z Data Source jest w przykładzie.
+        public static string connectionString => $"Data Source={dbPath}";
+    }
+}
