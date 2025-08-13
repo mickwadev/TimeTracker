e870489dbe437fcc653fcdb5284958b8beef0a7a
mickwa dev <mickwadev@gmail.com>
2025-08-13 16:52:47 +0200

    Chart values update works ^_^

--- PageModels/ChartTestPageViewModel.cs
+++ PageModels/ChartTestPageViewModel.cs
@@ -4,7 +4,6 @@ using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.Kernel.Sketches;
 using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
@@ -12,9 +11,24 @@ namespace TimeTracker.PageModels
 {
     public partial class ChartTestPageViewModel : ObservableObject
     {
+        // https://livecharts.dev/docs/maui/2.0.0-rc5.4/Overview.Automatic%20updates
         [ObservableProperty]
         ObservableCollection<int> _ints =new ObservableCollection<int>() {1,2,3};
-         
+        Random random = new Random();
+
+        public ISeries[] Series2 { get;}
+
+        public ChartTestPageViewModel()
+        {
+            // since _ints is of type ObservableCollection 
+            // LiveCharts will update when you add, remove, replace or clear the collection
+            LineSeries<int> lineSeries = new LineSeries<int>();
+            lineSeries.Values = Ints;
+
+
+            Series2 = new ISeries[] { new LineSeries<int>() { Values = Ints} };
+        }
+
         public ISeries[] Series { get; set; } = 
             [
                 new ColumnSeries<DateTimePoint>
@@ -36,7 +50,7 @@ namespace TimeTracker.PageModels
         public void AddValueButton()
         {
             Trace.WriteLine("Add value to chart...");
-            Ints.Add(0);
+            Ints.Add(random.Next(0,10));
              
             var d = new ColumnSeries<DateTimePoint>
             {
