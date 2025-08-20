3ff51191b57b7d1a4667f22454f45fc8fb2363de
mickwa dev <mickwadev@gmail.com>
2025-08-20 12:12:40 +0200

    Toggle

---
+++ Converters/InverseBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Converters
+{
+    internal class InverseBoolConverter : IValueConverter
+    {
+        object? IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        object? IValueConverter.ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
