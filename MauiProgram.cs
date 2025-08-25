af91d7b3dbbbbe9d388f73c08b4474837e09301f
mickwa dev <mickwadev@gmail.com>
2025-08-25 17:05:40 +0200

    Remove FontAwesome and MaterialIcons because it failes building on phone :/

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -29,8 +29,6 @@ namespace TimeTracker
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddMaterialSymbolsFonts();
-                    fonts.AddMaterialIconFonts();
-                    fonts.AddFontAwesomeIconFonts();
                 });
 
 #if DEBUG
--- TestPages/CollectionViewFun1.xaml.cs
+++ TestPages/CollectionViewFun1.xaml.cs
@@ -1,20 +1,15 @@
-using UraniumUI.Icons.MaterialSymbols;
-using UraniumUI.Icons.MaterialIcons;
+using UraniumUI.Icons.MaterialSymbols; 
 namespace TimeTracker.TestPages;
 
 public partial class CollectionViewFun1 : ContentPage
 {
 	public CollectionViewFun1()
-	{
-	
-
+	{ 
 		InitializeComponent();
 	}
 
 	private void D()
-	{
-		
-		var d2 = UraniumUI.Icons.MaterialIcons.MaterialRegular.Abc;
+	{ 
 		var d = UraniumUI.Icons.MaterialSymbols.MaterialOutlined.Abc;
 		var d3 = UraniumUI.Icons.MaterialSymbols.MaterialSharp.Abc;
     }
