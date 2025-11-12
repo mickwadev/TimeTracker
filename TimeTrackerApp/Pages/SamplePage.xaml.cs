80cf8fcae683c7cafb3d98916c0b2a3107abca7a
mickwa dev <mickwadev@gmail.com>
2025-11-12 16:07:45 +0100

    Remove label text

--- TimeTrackerApp/Pages/SamplePage.xaml.cs
+++ TimeTrackerApp/Pages/SamplePage.xaml.cs
@@ -8,20 +8,12 @@ public partial class SamplePage : ContentPage
 	public SamplePage(WorkTimesManager wtm)
 	{
 		InitializeComponent();
-		try
-		{
-			shellBkgLabel.Text = Shell.Current.BackgroundColor.ToString();
-		}
-		catch (Exception ex) 
-		{
-		 Trace.WriteLine(ex.Message);
-		}
+		
 	}
 
     private void Switch_Toggled(object sender, ToggledEventArgs e)
     {
 		var d = AppTheme.Dark;
 		Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
-        shellBkgLabel.Text = Shell.Current.BackgroundColor.ToString();
     }
 }
